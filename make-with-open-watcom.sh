71085777c243f225d221f1fad6e6a3558e95b139
Volkert de Buisonjé <volkertb@users.noreply.github.com>
2020-05-22 18:50:37 +0200

    Add script for building with Open Watcom assembler and linker, remove original EXE, add .gitignore

---
+++ make-with-open-watcom.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Important: make sure to source owsetenv.sh in your Open Watcom installation directory first, so that wasm and wlink
+# are available in the path. Otherwise this make script will fail.
+
+set -e
+
+if [ -f player.exe ]; then
+  rm player.exe
+fi
+
+wasm player.asm
+wasm cmdline.asm
+wasm codec.asm
+wasm file.asm
+wasm ichwav.asm
+wasm memalloc.asm
+wasm pci.asm
+wasm utils.asm
+
+wlink sys dos file player.o file cmdline.o file codec.o file file.o file ichwav.o file memalloc.o file pci.o file utils.o
+
+rm *.o
